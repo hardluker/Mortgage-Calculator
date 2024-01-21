1597c7a468504e67dc9bb34bc408c0bf3c80469f
hardluker <dba9514@gmail.com>
2024-01-21 15:28:32 -0500

    Refactoring and adding a method for printing the mortgage payment. Also added a method to print out a full payment schedule for the span of the mortgage.

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,20 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
+    final static byte PERCENT = 100;
+    final static byte MONTHS_IN_YEAR = 12;
     public static void main(String [] args) {
 
         //Getting Principal, Annual Interest Rate, and Years
         double principal = readNumber("Principal: ", 1_000.00, 1_000_000.00);
-        double annualInterestRate = readNumber("Annual Interest Rate: ", 0, 30);
-        double years = readNumber("Years: ", 1, 40);
+        float annualInterestRate = (float)readNumber("Annual Interest Rate: ", 0, 30);
+        byte years = (byte)readNumber("Years: ", 1, 40);
 
-        //Calculating the mortgage payment
-        double mortgagePayment = calculateMortgagePayment(principal, annualInterestRate, years);
+        //Calculating and printing the mortgage payment
+        printMortgage(principal, annualInterestRate, years);
 
-        //Formatting and printing mortgage payment
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgagePayment);
-        System.out.println("Monthly payment is " + mortgageFormatted);
+        printPaymentSchedule(years, principal, annualInterestRate);
     }
-
-
 
     //Method for getting values from the user input
     public static double readNumber(String prompt, double min, double max) {
@@ -38,16 +36,50 @@ public class Main {
 
 
 
+    //Method for calculating the balance through the iterations of the payment schedule
+    public static double calculateBalance(double principal, float annualInterestRate, byte years, short numberOfPaymentsMade) {
+        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
+
+        return principal
+                *(Math.pow(1 + monthlyInterestRate, numberOfPayments) - Math.pow(1 + monthlyInterestRate, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+    }
+
+
     //Method for calculating Mortgage payment.
-    public static double calculateMortgagePayment(double principal, double AnnualInterestRate, double years) {
+    public static double calculateMortgagePayment(double principal, float annualInterestRate, byte years) {
         //Defining the monthly interest rate
-        final float PERCENT = 100.00F;
-        final int MONTHS_IN_YEAR = 12;
-        double monthlyInterestRate = AnnualInterestRate / PERCENT / MONTHS_IN_YEAR;
+        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+        float numberOfPayments = years * MONTHS_IN_YEAR;
 
         //Calculating the monthly payment
-        return (principal * ((monthlyInterestRate * Math.pow(1.00 + monthlyInterestRate, years * 12))
-                /(Math.pow(1.00 + monthlyInterestRate, years * 12) - 1)));
+        return (principal * ((monthlyInterestRate * Math.pow(1.00 + monthlyInterestRate, numberOfPayments))
+                /(Math.pow(1.00 + monthlyInterestRate, numberOfPayments) - 1)));
 
     }
+
+    //Method for formatting and printing the mortgage payment
+    public static void printMortgage(double principal, float annualInterestRate, byte years) {
+        double mortgagePayment = calculateMortgagePayment(principal, annualInterestRate, years);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgagePayment);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly payment: " + mortgageFormatted);
+    }
+
+
+    //Method for formatting and printing the payment schedule
+    public static void printPaymentSchedule(byte years, double principal, float annualInterestRate) {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
+            double balance = calculateBalance(principal, annualInterestRate, years, month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+
+
 }
