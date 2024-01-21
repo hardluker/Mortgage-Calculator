faf9ce5fda368979f2d1b596c96f11f9f8e46f10
hardluker <dba9514@gmail.com>
2024-01-21 14:15:34 -0500

    Adding error handling for value ranges.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args) {
-
         //Declaring constants to avoid magic numbers
         final float PERCENT = 100.00F;
         final int MONTHS_IN_YEAR = 12;
@@ -11,17 +10,46 @@ public class Main {
         //Creating the scanner
         Scanner scanner = new Scanner(System.in);
 
-        //Getting Principal Value
-        System.out.print("Principal: ");
-        double principal = scanner.nextInt();
+        //Getting Principal Value with error handling
+        double principal;
+        while(true){
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
 
-        //Getting Interest rate
-        System.out.print("Interest Rate: ");
-        double monthlyInterestRate = (scanner.nextDouble() / PERCENT / MONTHS_IN_YEAR);
+            //Confirming principal is between 1,000 and 100,000,000.
+            if (principal >= 1_000.00 && principal <= 100_000_000.00){
+                break;
+            }
+                System.out.println("Principal must be between 1,000 - 100,000,000");
+        }
 
-        //Getting the period in years
-        System.out.print("Number of Years: ");
-        double years = scanner.nextInt();
+
+        //Getting Interest rate with error handling.
+        double monthlyInterestRate;
+        while (true) {
+            System.out.print("Interest Rate: ");
+            double interestRate = scanner.nextDouble();
+
+            //Confirming interest rate is between 0 and 30 otherwise.
+            if (interestRate > 0.00 && interestRate <= 30.00) {
+                monthlyInterestRate = (interestRate / PERCENT / MONTHS_IN_YEAR);
+                break;
+            }
+                System.out.println("Interest rate must be between 0.00 - 30.00");
+        }
+
+        //Getting the period in years.
+        double years;
+        while (true) {
+            System.out.print("Number of Years: ");
+            years = scanner.nextDouble();
+
+            //Confirming years is between 1 and 40.
+            if (years >= 1.00 && years <= 40.00) {
+                break;
+            }
+            System.out.println("Years must be between 1 - 40");
+        }
 
         //Running method for monthly payment calculation
         double monthlyPayment = calculateMortgagePayment(principal, monthlyInterestRate, years);
