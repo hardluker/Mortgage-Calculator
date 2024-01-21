d0e2b79af29e7362d13a4a6834c0cf03c3d7b485
hardluker <dba9514@gmail.com>
2024-01-21 12:56:39 -0500

    First Commit.

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args) {
+
+        //Declaring constants to avoid magic numbers
+        final float PERCENT = 100.00F;
+        final int MONTHS_IN_YEAR = 12;
+
+        //Creating the scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //Getting Principal
+        System.out.print("Principal: ");
+        double principal = (double)scanner.nextInt();
+
+        //Getting Interest rate
+        System.out.print("Interest Rate: ");
+        double monthlyInterestRate = (scanner.nextDouble() / PERCENT / MONTHS_IN_YEAR);
+
+        //Getting the period in years
+        System.out.print("Number of Years: ");
+        double years = (double)scanner.nextInt();
+
+        //Running method for monthly payment calculation
+        double monthlyPayment = calculateMortgagePayment(principal, monthlyInterestRate, years);
+
+        //Formatting and printing monthly payment
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(monthlyPayment);
+        System.out.println("Payment is " + mortgageFormatted);
+
+
+
+    }
+
+    //Method for calculating monthly payment.
+    static double calculateMortgagePayment(double principal, double interestRate, double years) {
+        return (principal * ((interestRate * Math.pow(1.00 + interestRate, years * 12))
+                /(Math.pow(1.00 + interestRate, years * 12) - 1)));
+    }
+}
